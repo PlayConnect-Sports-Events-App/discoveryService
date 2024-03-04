d396a9f1bb5ab4c5099a144190b1d449f3094a6d
Lachezar Mitov <lachezarspam1@gmail.com>
2024-03-04 18:46:14 +0100

    Add skeleton of the Discovery Service

---
+++ src/test/java/com/playconnect/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playconnect.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
