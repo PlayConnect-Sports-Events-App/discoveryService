576141f44d4bb616decbfe7251b45b3100eb5686
Lachezar Mitov <lachezarspam1@gmail.com>
2024-05-29 22:30:16 +0200

    Fix SSL certificate issue

--- src/main/java/com/playconnect/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/com/playconnect/discoveryservice/DiscoveryServiceApplication.java
@@ -7,9 +7,14 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
-
 	public static void main(String[] args) {
+		try {
+			// Call the method to disable SSL validation
+			DisableSSLValidation.disable();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
-
 }
